3b1be9d8076b9ae1c9dc72da49f0b8eb3ac075bc
Igor Guryan <sacujo93@yandex.ru>
2022-07-24 17:26:15 +0300

    Initial Commit

---
+++ Project1_HackingWithSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1_HackingWithSwift
+//
+//  Created by Igor Guryan on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
