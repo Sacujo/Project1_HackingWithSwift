00860f7e0c8727d2b0b1082e326554d728f6646d
Igor Guryan <sacujo93@yandex.ru>
2022-10-24 22:50:46 +0300

    Create DetailViewCntroller with ImageView

---
+++ Project1_HackingWithSwift/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  Project1_HackingWithSwift
+//
+//  Created by Igor Guryan on 24.10.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
